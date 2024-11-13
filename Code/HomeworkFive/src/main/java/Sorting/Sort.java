6be948a0313f8ca23dbd8820e1711a8dec69ef15
Trevor Swan <tcs94@case.edu>
2024-11-13 14:11:18 -0500

    fixed as5 shit

---
+++ Code/HomeworkFive/src/main/java/Sorting/Sort.java
@@ -0,0 +1,20 @@
+package Sorting;
+
+/**
+ * Required sorting methods for Assignment 5
+ * 
+ * @author Trevor Swan
+ * @version CSDS233 - Fall 2024
+ */
+public class Sort {
+    
+    /**
+     * The entry method for this class to test the sorting methods
+     * 
+     * @param args The command line arguments
+     */
+    public static void main(String[] args) {
+        // The specified array to be sorted
+        
+    }
+}
